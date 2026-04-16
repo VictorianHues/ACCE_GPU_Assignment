57b4b2f90d78f194d452c4567de30b4695b94eb5
Karolina Chlopicka <tln716@fs0.das5.cs.vu.nl>
2026-04-16 18:53:29 +0200

    Cloud movement

--- flood_cuda.cu
+++ flood_cuda.cu
@@ -67,6 +67,26 @@ __global__ void init_state_kernel(int rows, int columns, int *d_water_level, flo
     }
 }
 
+__global__ void cloud_movement(
+    int *p->num_clouds // or maybe I should pass only p?
+){
+    // each thread must be assigned a separate cloud to compute
+    // this way we are waisting some of the threads - we only use first 
+    cloud = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (cloud > p->num_clouds) return;
+
+    /* Step 1.1: Clouds movement */
+    // Calculate new position (x are rows, y are columns)
+    Cloud_t *c_cloud = &p->clouds[cloud];
+    float km_minute = c_cloud->speed / 60;
+    c_cloud->x += km_minute * cos(c_cloud->angle * M_PI / 180.0);
+    c_cloud->y += km_minute * sin(c_cloud->angle * M_PI / 180.0);
+}
+
+
+
+
 __global__ void cloud_movement_kernel(int num_clouds, Cloud_t *d_clouds) {
     int cloud_idx = blockIdx.x * blockDim.x + threadIdx.x;
 
