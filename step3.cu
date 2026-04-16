b140b1c71ea0b16bf0ea85f6769d1876e5ddbbea
Karolina Chlopicka <tln716@fs0.das5.cs.vu.nl>
2026-04-16 15:52:17 +0200

    kernel 3 implementation

--- step3.cu
+++ step3.cu
@@ -3,6 +3,7 @@ __global__ void step3_kernel(
     float *spillage_flag,
     float *spillage_level,
     float *spillage_from_neigh,
+    int *max_spillage_iter,
     int rows,
     int columns
 ) {
@@ -11,13 +12,18 @@ __global__ void step3_kernel(
 
     if (row_pos >= rows || col_pos >= columns) return;
 
-    int depths = CONTIGUOUS_CELLS; // needed?
+    int depths = CONTIGUOUS_CELLS;
     int cell_pos;
 
     if (accessMat(spillage_flag, row_pos, col_pos) == 1) {
         accessMat(water_level, row_pos, col_pos) -=
             FIXED(accessMat(spillage_level, row_pos, col_pos) / SPILLAGE_FACTOR);
     }
+
+        // Compute termination condition: Maximum cell spillage during the iteration
+        // Note: atomic max works only for floats 
+        int current_spill = FIXED(accessMat(spillage_level, row_pos, col_pos) / SPILLAGE_FACTOR);
+        atomicMax(max_spillage_iter, current_spill); 
 
     for (cell_pos = 0; cell_pos < CONTIGUOUS_CELLS; cell_pos++) {
         accessMat(water_level, row_pos, col_pos) +=
