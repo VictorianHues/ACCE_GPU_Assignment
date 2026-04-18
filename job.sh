19e3d652fdecaf141491ae03bfd612bfe0616512
karolina-chl <k.m.chlopicka@student.vu.nl>
2026-04-18 23:04:00 +0200

    stride implementation

--- job.sh
+++ job.sh
@@ -9,5 +9,11 @@
 #SBATCH --constraint="gpunode,TitanX"
 #SBATCH --gres=gpu:1
 
+<<<<<<< Updated upstream
 ## This is an example of a SLURM job script to run the program on a GPU node
 ./flood_cuda $(< test_files/debug.in)
+=======
+#./flood_seq $(< test_files/large_mountains.in)
+#nvprof ./flood_cuda $(< test_files/large_mountains.in)
+nvprof ./flood_cuda_soa $(< test_files/large_mountains_extreme.in)
+>>>>>>> Stashed changes
