330b0f8f92448d19bf74d69ab42175ef1841ad41
Marcell Szegedi <szegedi.marcell@gmail.com>
2026-04-18 20:48:24 +0200

    job.sh change to test with diff cloud settings

--- job.sh
+++ job.sh
@@ -10,4 +10,5 @@
 #SBATCH --gres=gpu:1
 
 ## This is an example of a SLURM job script to run the program on a GPU node
-nvprof ./flood_cuda_soa $(< test_files/large_mountains.in)
+nvprof ./flood_cuda_soa 3072 2048 M 0.0000001 800 10 30 70 45 240 80 3 80 32 35 83766
+nvprof ./flood_cuda_soa 3072 2048 M 0.0000001 800 10 30 70 45 240 800 0.1 80 32 35 83766
