cb512a45ae31218f416f12be3934de066fde97af
Marcell Szegedi <szegedi.marcell@gmail.com>
2026-04-18 23:06:37 +0200

    changed job.sh file

--- job.sh
+++ job.sh
@@ -10,7 +10,4 @@
 #SBATCH --gres=gpu:1
 
 ## This is an example of a SLURM job script to run the program on a GPU node
-./flood_seq $(< test_files/large_mountains.in)
-nvprof ./flood_cuda $(< test_files/large_mountains.in)
-
 nvprof ./flood_cuda_soa $(< test_files/large_mountains.in)
